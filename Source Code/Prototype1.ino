a29f8ba2b2afef2c237e95920401be748e05e358
Koushik Chowdhury <45112318+chykoushik@users.noreply.github.com>
2020-07-03 11:58:00 +0200

    ISystem Arduino

--- Source Code/Prototype1.ino
+++ Source Code/Prototype1.ino
@@ -17,7 +17,7 @@ void loop() {
   pinMode(pingPin, INPUT);
   duration = pulseIn(pingPin, HIGH);
 
-  // convert the time into a distance
+  
   inches = microsecondsToInches(duration);
   cm = microsecondsToCentimeters(duration);
 
@@ -35,8 +35,6 @@ long microsecondsToInches(long microseconds) {
 }
 
 long microsecondsToCentimeters(long microseconds) {
-  // The speed of sound is 340 m/s or 29 microseconds per centimeter.
-  // The ping travels out and back, so to find the distance of the object we
-  // take half of the distance travelled.
+  
   return microseconds / 29 / 2;
 }
